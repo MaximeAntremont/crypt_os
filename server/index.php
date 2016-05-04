f0ec53fbe39260d4485db1508a6c1ef65ad0453e
MaximeAntremont <maxime.antremont@gmail.com>
2016-05-04 11:31:42 +0200

    Ajout du player en place avec gif de chargement

--- server/index.php
+++ server/index.php
@@ -1,1 +1,17 @@
-"" 
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <video poster="./assets/img/load.gif" preload="auto" muted="muted" autoplay name="media" width="400" height="300">
+        Ce navigateur ne supporte pas la vidéo...
+    </video>
+</body>
+</html>
---
+++ server/record.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * Ici on va lancer ffmpeg pour enregistrer la video, puis on va retourner l'url du streaming
+ */
