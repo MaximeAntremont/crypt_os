01918934197a32cc55815657dc82371b94c68162
MaximeAntremont <maxime.antremont@gmail.com>
2016-05-09 10:43:07 +0200

    Enregistrement correct de la vidéo

--- server/assets/js/main.js
+++ server/assets/js/main.js
@@ -1,28 +1,35 @@
 /**
  * Created by maxim on 09/05/2016.
  */
+navigator.getUserMedia = ( navigator.getUserMedia ||
+navigator.webkitGetUserMedia ||
+navigator.mozGetUserMedia ||
+navigator.msGetUserMedia ||
+navigator.mediaDevices.getUserMedia);
+
 
 var body = document.querySelector("body"),
     start_btn = document.querySelector("#start"),
     stop_btn = document.querySelector("#stop"),
     video = document.querySelector("video"),
-    webmVideoRecorder;
+    webmVideoRecorder,
+    stream;
 
 
-function successCallback(stream) {
+function successCallback(_stream) {
+    stream = _stream;
     webmVideoRecorder = RecordRTC(stream, {
+        mimeType: 'video/webm',
         type: 'video'
     });
-
-    webmVideoRecorder.initRecorder(function() {
-        webmVideoRecorder.startRecording();
-    });
+    webmVideoRecorder.startRecording();
 
     video.src = URL.createObjectURL(stream);
 }
 
 function errorCallback(error) {
     // maybe another application is using the device
+    console.log(error);
 }
 
 var mediaConstraints = { video: true, audio: false };
@@ -32,9 +39,39 @@ start_btn.onclick = function(){
 };
 
 stop_btn.onclick = function () {
-    webmVideoRecorder.stopRecording(function() {
-            video.controls = true;
-            video.src = URL.createObjectURL(webmVideoRecorder.blob);
+    webmVideoRecorder.stopRecording(function(url) {
+            //video.controls = true;
+            video.src = url;
+        var blob = webmVideoRecorder.blob;
+        saveToFile(blob);
+        stream.stop();
     });
 };
+
+function xhr(url, data, callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            callback(location.href + "ress/" + request.responseText);
+        }
+    };
+    request.open('POST', url);
+    request.send(data);
+}
+
+function saveToFile(blob){
+
+    var fileType = blob.type.match(/([a-z]+)\//)[1];
+    var fileName = new Date().getTime().toString() + "." + blob.type.match(/\/([a-z]+)/)[1]; //Quand on coupe le record on prend le timestamp
+
+    var formData = new FormData();
+    formData.append(fileType + '-filename', fileName);
+    formData.append(fileType + '-blob', blob);
+
+    xhr('save.php', formData, function (fName) {
+        //window.open(location.href + fName);
+        console.log(fName);
+    });
+}
+
 
--- server/record.php
+++ server/record.php
@@ -1,4 +1,5 @@
 <?php
 /**
- * Ici on va lancer ffmpeg pour enregistrer la video, puis on va retourner l'url du streaming
+ * Ici on va lancer ffmpeg pour enregistrer le son, puis on va retourner l'url
  */
+
