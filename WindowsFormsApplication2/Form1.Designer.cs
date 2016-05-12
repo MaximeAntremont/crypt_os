bfb948f27fd4d831093c03efc3dcbaf3426c0584
MaximeAntremont <maxime.antremont@gmail.com>
2016-05-12 16:45:21 +0200

    Solution en C# avec vs2013 et le composant com de vlc ainsi que ffmpeg

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(12, 12);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(1280, 720);
+            this.axVLCPlugin21.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1307, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1307, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Play";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1307, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Record";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1393, 744);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        static Process process = null;
+        static int lineCount = 0;
+        static AxAXVLC.AxVLCPlugin2 player;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void ExecuteCommand(string command)
+        {
+            int exitCode;
+            ProcessStartInfo processInfo;
+            process = new Process();
+
+            processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            // *** Redirect the output ***
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            //Thread threadObj = new Thread(new ThreadStart(() => ConverToPdf("a", "b")));
+            //threadObj.Start();
+
+            process.EnableRaisingEvents = true;
+
+            process.ErrorDataReceived +=
+        new DataReceivedEventHandler(process_ErrorDataReceived);
+            process.OutputDataReceived +=
+                new DataReceivedEventHandler(process_OutputDataReceived);
+            process.Exited += new EventHandler(process_Exited);
+
+            process.StartInfo = processInfo;
+
+            process.Start();
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            //process.WaitForExit();
+
+            /*Thread.Sleep(2000);
+            player.playlist.add("D:/Documents/Visual Studio 2013/Projects/WindowsFormsApplication2/WindowsFormsApplication2/bin/Debug/files", null, null);
+            player.playlist.play();*/
+
+
+            // *** Read the streams ***
+            // Warning: This approach can lead to deadlocks, see Edit #2
+            //process.Close();
+        }
+
+        private static void process_Exited(object sender, EventArgs e)
+        {
+            process.Dispose();
+            Console.WriteLine("Bye bye!");
+        }
+
+        private static void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            //Console.WriteLine("Output Data Received.");
+        }
+
+        private static void process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            /*Console.WriteLine("Input line: {0} ({1:m:s:fff})", lineCount++,
+      DateTime.Now);
+            Console.WriteLine(e.Data);
+            Console.WriteLine();*/
+            if(e.Data == "Press [q] to stop, [?] for help"){
+                Console.WriteLine("Record started !");
+                player.playlist.add("udp://@127.0.0.1:1234/", "udp://@127.0.0.1:1234/", null);
+                player.playlist.play();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if(openFileDialog1.ShowDialog() == DialogResult.OK){
+                Console.WriteLine(openFileDialog1.FileName);
+                Console.WriteLine(openFileDialog1.SafeFileName);
+                axVLCPlugin21.playlist.add(openFileDialog1.FileName, openFileDialog1.SafeFileName, null);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            player = axVLCPlugin21;
+            ExecuteCommand("D:/Documents/_DEV/Confluence/ffmpeg_junks/record_audio_video_avi.bat");
+        }
+    }
+}
