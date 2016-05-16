7279599f70e9ddfdc9e06e19fe03371375423701
MaximeAntremont <maxime.antremont@gmail.com>
2016-05-16 17:26:11 +0200

    Affichage général des devices

--- show_devices_and_options.bat
+++ show_devices_and_options.bat
@@ -1,2 +1,4 @@
+ffmpeg -list_devices true -f dshow -i dummy
+pause
 ffmpeg -f dshow -list_options true -i video="@device_pnp_\\?\usb#vid_5986&pid_055c&mi_00#6&38cd688a&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\global":audio="Microphone (Realtek High Definition Audio)"
 pause
